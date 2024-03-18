3228a10e5b1fdb752da7a370f4a67fdea4849978
rodrigorocha1 <silva.rodrigo31@outlook.com>
2024-03-17 21:46:12 -0300

    tipagem no navegador

--- src/dados/service_selenium.py
+++ src/dados/service_selenium.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.webdriver import WebDriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -16,7 +17,7 @@ class WebScrapingSelenuium:
         self._url = url
         self._servico = Service(ChromeDriverManager().install())
 
-    def __clicar_cookie(self, navegador):
+    def __clicar_cookie(self, navegador: WebDriver):
         WebDriverWait(navegador, 10).until(EC.element_to_be_clickable(
             (By.XPATH, '//*[@id="cookie-notifier-cta"]'))).click()
 
@@ -26,7 +27,7 @@ class WebScrapingSelenuium:
         self.__clicar_cookie(navegador)
         return navegador
 
-    def extrair_dados(self, navegador) -> List[Tuple[WebElement, WebElement, WebElement, WebElement, WebElement, WebElement, WebElement, WebElement]]:
+    def extrair_dados(self, navegador: WebDriver) -> List[Tuple[WebElement, WebElement, WebElement, WebElement, WebElement, WebElement, WebElement, WebElement]]:
 
         lista_ids = navegador.find_elements(
             By.CLASS_NAME, 'property-card__content-link')
@@ -54,7 +55,7 @@ class WebScrapingSelenuium:
 
         return zip(lista_ids, nome_apartamentos, precos, enderecos_apartamentos, metragem, quartos, banheiros, garagem)
 
-    def executar_paginacao(self, navegador):
+    def executar_paginacao(self, navegador: WebDriver):
         try:
             navegador.find_element(
                 By.XPATH,  '//*[@id="js-site-main"]/div[2]/div[1]/section/div[2]/div[2]/div/ul/li[9]/button').click()
@@ -62,5 +63,5 @@ class WebScrapingSelenuium:
         except ElementClickInterceptedException:
             return False
 
-    def fechar_navegador(self, navegador):
+    def fechar_navegador(self, navegador: WebDriver):
         navegador.quit()
